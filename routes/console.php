b6a1ea41c034ed7144fc3596dc64c4ae919aed30
anindyoy <anindyo.yudhistiro@gmail.com>
2026-09-06 00:12:33 +0700

    fix: jalankan scheduler tanpa proc_open

--- routes/console.php
+++ routes/console.php
@@ -6,12 +6,14 @@ use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Display an inspiring quote');
 
-Schedule::command('masa-aktif:kirim-pengingat')
+Schedule::call(fn (): int => Artisan::call('masa-aktif:kirim-pengingat'))
+    ->name('masa-aktif:kirim-pengingat')
     ->dailyAt('08:00')
     ->withoutOverlapping();
 
-Schedule::command('telescope:prune --hours=336')
+Schedule::call(fn (): int => Artisan::call('telescope:prune', ['--hours' => 336]))
+    ->name('telescope:prune')
     ->daily()
     ->withoutOverlapping();
